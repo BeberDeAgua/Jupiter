65791aacb0808989f4994936c5ee82a2ab1b81e5
BeberDeAgua <57802753+BeberDeAgua@users.noreply.github.com>
2019-11-20 12:02:54 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var dotenv = require('dotenv');
-dotenv.config();
+//var dotenv = require('dotenv');
+//dotenv.config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
