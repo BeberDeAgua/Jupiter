c7879e0767651ce34bd66369ddcb02c5da28369a
BeberYComer <57978441+BeberYComer@users.noreply.github.com>
2019-11-20 16:18:24 -0600

    Update main.js

--- main.js
+++ main.js
@@ -8,14 +8,9 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    var rawmessage = msg.content;
-    var i;
-    var command = "";
-    for (i = 0; rawmessage.charAt(i) != " "; i++) {
-        command = command + rawmessage.charAt(i)
-    };
+    var rawmsg = msg.content;
     console.log(command);
-    if (msg.slice(0, 1) = process.env.PREFIX) {
+    if (rawmsg.slice(0, 1) == process.env.PREFIX) {
       if (command === 'ping') {
         msg.reply('pong');
       }
