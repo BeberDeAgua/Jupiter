703d7ccd1be019cf10f31069ce453e79bab22c7a
BeberDeAgua <57802753+BeberDeAgua@users.noreply.github.com>
2019-11-22 11:17:14 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var dotenv = require('dotenv');
 dotenv.config();
+const Request = require('request');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -8,17 +9,24 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    var rawmessage = msg.content;
-    var i
-    var command = ""
-    for (i = 0, rawmessage.charAt(i) !== " ", i++) {
-        command + rawmessage.charAt(i)
-    }
-    if (msg.slice(0, 1) = process.env.PREFIX) {
+    var rawmsg = msg.content;
+    var command = rawmsg.slice(1);
+    console.log(rawmsg);
+    if (rawmsg.slice(0, 1) == process.env.PREFIX) {
       if (command === 'ping') {
-        msg.reply('pong');
+        msg.reply('Pong.');
+      } else if (command == 'dmMe') {
+        msg.author.send("Hi, I've DMed you.")
+      } else if (command == 'verifyMe') {
+        msg.author.send("Working on it.");
+        
       }
-   )
+    };
+});
+
+request.get('https://jupiter-0.herokuapp.com/get', {
+    'Lmao what' : 'lmbao'
 });
 
 client.login();
+
