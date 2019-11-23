1ad4046e0a772a02e7e0f18e3abeee0eade06ea7
BeberYComer <57978441+BeberYComer@users.noreply.github.com>
2019-11-23 01:02:29 -0600

    Update Mongo.js

--- MongoDB/Mongo.js
+++ MongoDB/Mongo.js
@@ -1,11 +1,22 @@
 var dotenv = require('dotenv');
 dotenv.config();
+const http = require('http');
+const JSONbody = require('body/json');
+const Body = require('body/form');
 const Mongo = require('mongodb');
 var url = "mongodb+srv://Joe:" + process.env.MONGOPASS + "@clu-ster-99b4b.azure.mongodb.net/test?retryWrites=true&w=majority";
 
-const client = new Mongo.MongoClient(url, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
+const client = new Mongo.MongoClient(url, { useUnifiedTopology: true });
+client.connect(url, function(err, db) {
+  if (err) throw err;
+  const Codes = client.db("Jupiter").collection("Codes");
+  
+  const server = http.createServer(function(req, res) {
+    var jsbody = yield JSONbody(req, res);
+    var body = yield Body(req, res);
+    
+    console.log(jsbody)
+  });
+  
   client.close();
 });
