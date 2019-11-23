0a4bdc919e235adb501c3b3cc51bec25c03492b4
BeberYComer <57978441+BeberYComer@users.noreply.github.com>
2019-11-23 01:26:38 -0600

    Update Mongo.js

--- MongoDB/Mongo.js
+++ MongoDB/Mongo.js
@@ -7,7 +7,7 @@ const Mongo = require('mongodb');
 var url = "mongodb+srv://Joe:" + process.env.MONGOPASS + "@clu-ster-99b4b.azure.mongodb.net/test?retryWrites=true&w=majority";
 
 const client = Mongo.MongoClient;
-client.connect(url, (err, db) {
+client.connect(url, function(err, db) {
   const Codes = client.db("Jupiter").collection("Codes");
   
   const server = http.createServer(function(req, res) {
