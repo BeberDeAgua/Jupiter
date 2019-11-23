038fb514c54ef00ebc402b0924ab382f7ed27c5a
BeberYComer <57978441+BeberYComer@users.noreply.github.com>
2019-11-22 18:58:17 -0600

    Update sqlite.js

--- SQLite/sqlite.js
+++ SQLite/sqlite.js
@@ -3,6 +3,10 @@ let Database = new sqlite.Database('./SQLite/OWO.db', (err) => {
   if (err) {
     console.log(err.message)
   };
-}
+};
+                                   
+Database.run('CREATE TABLE codes(discordUserID accountName code)');
+
+
 
 Database.close();
