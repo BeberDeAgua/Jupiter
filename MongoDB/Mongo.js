4c5d863805d6e0b9b804647c0dd2e8267e167c4b
BeberYComer <57978441+BeberYComer@users.noreply.github.com>
2019-11-23 22:10:48 -0600

    Update Mongo.js

--- MongoDB/Mongo.js
+++ MongoDB/Mongo.js
@@ -5,45 +5,44 @@ const JSONbody = require('body/json');
 const Body = require('body/form');
 const Mongo = require('mongodb').MongoClient;
 var url = "mongodb+srv://Joe:" + process.env.MONGOPASS + "@clu-ster-99b4b.azure.mongodb.net/test?retryWrites=true&w=majority";
-
-
+           
+function DatabaseSave(db, thingToSave) {
+  db.insertOne(thingToSave, function(err, res) {
+    if (err) throw err;
+    console.log(res);
+  }
+};
+               
+function DatabaseGet(db, thingToGet) {
+  db.findOne(thingToGet, function(err, res) {
+    if (err) throw err;
+    return res;
+  }
+};
 
 Mongo.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
   if (err) throw err;
   const DB = client.db("Jupiter");
   const Codes = DB.collection("Codes");
   
+  const server = http.createServer(function(request, response) {
+    console.dir(request.param);
+
+    if (request.method == 'GET') {
+      console.log('GET');
+      if (request.ID) {
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        var thingy = DatabaseGet(Codes, request.ID)
+        if (thingy) {
+          response.end();
+        };
+      };
+    } ;
+    
+  });
+  
   client.close();
 });
 
-const server = http.createServer(function(request, response) {
-  console.dir(request.param)
 
-  if (request.method == 'POST') {
-    console.log('POST')
-    var body = ''
-    request.on('data', function(data) {
-      body += data
-      console.log('Partial body: ' + body)
-    })
-    request.on('end', function() {
-      console.log('Body: ' + body)
-      response.writeHead(200, {'Content-Type': 'text/html'})
-      response.end('post received')
-    })
-  } else {
-    console.log('GET')
-    var html = `
-            <html>
-                <body>
-                    <form method="post">Name: 
-                        <input type="text" name="name" />
-                        <input type="submit" value="Submit" />
-                    </form>
-                </body>
-            </html>`
-    response.writeHead(200, {'Content-Type': 'text/html'})
-    response.end(html)
-  }
-});
 server.listen(process.env.PORT);
