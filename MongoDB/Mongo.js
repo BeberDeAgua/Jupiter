f6aec1bce465265fe015db9d66b93d4fd7f33420
BeberYComer <57978441+BeberYComer@users.noreply.github.com>
2019-11-23 19:18:30 -0600

    Update Mongo.js

--- MongoDB/Mongo.js
+++ MongoDB/Mongo.js
@@ -16,7 +16,34 @@ Mongo.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, function(e
   client.close();
 });
 
-const server = http.createServer(function(req, res) {
-    console.log("AAAAA");
+const server = http.createServer(function(request, response) {
+  console.dir(request.param)
+
+  if (request.method == 'POST') {
+    console.log('POST')
+    var body = ''
+    request.on('data', function(data) {
+      body += data
+      console.log('Partial body: ' + body)
+    })
+    request.on('end', function() {
+      console.log('Body: ' + body)
+      response.writeHead(200, {'Content-Type': 'text/html'})
+      response.end('post received')
+    })
+  } else {
+    console.log('GET')
+    var html = `
+            <html>
+                <body>
+                    <form method="post" action="http://localhost:3000">Name: 
+                        <input type="text" name="name" />
+                        <input type="submit" value="Submit" />
+                    </form>
+                </body>
+            </html>`
+    response.writeHead(200, {'Content-Type': 'text/html'})
+    response.end(html)
+  }
 });
 server.listen();
