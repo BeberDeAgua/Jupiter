1e421c4ad3b4dcfabe0c6d33bb0bf9843ef253f0
BeberYComer <57978441+BeberYComer@users.noreply.github.com>
2019-11-25 01:08:53 -0600

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,38 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+const HelpEmbed = {
+	color: 0xff5959,
+	title: 'Hi!',
+	description: "This is the list of Jupiter's commands. The bot itself is not very useful for purposes such as moderation, but it does help with Roblox - Discord verification.",
+	thumbnail: {
+		url: 'https://i.imgur.com/wSTFkRM.png',
+	},
+	fields: [
+		{
+			name: 'E',
+			value: 'Some value here',
+		},
+		{
+			name: 'A',
+			value: 'Some value here',
+			inline: true,
+		},
+		{
+			name: 'Sports',
+			value: 'Some value here',
+			inline: true,
+		},
+	],
+	timestamp: new Date(),
+	footer: {
+		text: 'Jupiter, made with love~',
+		icon_url: 'https://imgur.com/kLfFnBD',
+	},
+};
+
+
+
 client.on('message', msg => {
     const warnmsg = "Nothing to work with here. Missing arguments."
     var rawmsg = msg.content;
@@ -16,11 +48,11 @@ client.on('message', msg => {
     if (rawmsg.slice(0, 1) == process.env.PREFIX) {
       if (command === 'ping') {
         msg.reply('Pong.');
-      } else if (command == 'dmMe') {
-        msg.author.send("Hi, I've DMed you.")
+      } else if (command == 'help') {
+        msg.author.send(  
       } else if (command == 'getCode') {
           if (args[0]) {
-              msg.author.send("Working on connecting accountID \`" + args[0] + "\` to your account, " + msg.author.username + ".");
+              msg.author.send("Working on connecting username \`" + args[0] + "\` to your account, " + msg.author.username + ".");
               const data = JSON.stringify({thing: args[0], discName: msg.author.username});
               
               var req = https.request("https://jupiter-0.herokuapp.com/", {method: "POST"});
