13a0489506fd43b0afbe63282d18d42b25c96432
BeberDeAgua <57802753+BeberDeAgua@users.noreply.github.com>
2019-11-25 12:52:02 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var dotenv = require('dotenv');
 dotenv.config();
+const event = require('./MongoDB/Mongo.js');
 const Discord = require('discord.js');
 const https = require('https');
 const client = new Discord.Client();
@@ -51,6 +52,24 @@ client.on('message', msg => {
       } else if (command == 'help') {
         msg.author.send({embed: helpEmbed});
       } else if (command == 'getCode') {
+        if (args[0]) {
+              msg.author.send("Getting code for \`" + args[0] + "\`, " + msg.author.username + ". . .");
+              const data = JSON.stringify({thing: args[0], discName: msg.author.username});
+              
+              var req = https.request("https://jupiter-0.herokuapp.com/", {method: "GET"});
+              req.write(data);
+              req.end();
+              event.on('Success', function(code){
+                msg.author.send("Your code for \`" + args[0] + "\` is \`" + code + "\`.");
+              };
+              event.on('Faliure', function(code){
+                msg.author.send("Code not found for user or you aren't the verified Discord account, pal.");
+              }
+
+          } else {
+              msg.author.send(warnmsg);
+          }
+      } else if (command == 'makeCode') {
           if (args[0]) {
               msg.author.send("Working on connecting username \`" + args[0] + "\` to your account, " + msg.author.username + ". . .");
               const data = JSON.stringify({thing: args[0], discName: msg.author.username});
@@ -58,6 +77,14 @@ client.on('message', msg => {
               var req = https.request("https://jupiter-0.herokuapp.com/", {method: "POST"});
               req.write(data);
               req.end();
+              var succ = event.on('Success', function(code){
+                msg.author.send("Made code: \`" + code + "\` and linked accounts. Do ***not*** share this code with anyone else, please.");
+                succ.off('Success');
+              };
+              var fail = event.on('Faliure', function(code){
+                msg.author.send("Code already exists for this user. If this is a mistake, please use the command \`~resetCode <RobloxUsername>\` to delete any connections made.");
+                fail.off('Failure');
+              };
 
           } else {
               msg.author.send(warnmsg);
