e52407d3338f7dcca72ee225c339ddaaa1ddf9fd
BeberYComer <57978441+BeberYComer@users.noreply.github.com>
2019-11-24 18:51:42 -0600

    Update Mongo.js

--- MongoDB/Mongo.js
+++ MongoDB/Mongo.js
@@ -53,7 +53,7 @@ Mongo.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, function(e
           var options = {method: 'HEAD', host: 'roblox.com', path: '/users/' + table.thing};
           console.log(options.path);
           req = http.request(options, function(r) {
-             console.log(JSON.stringify(r.status));
+             console.log(JSON.stringify(r.headers));
           });
           req.end();
 
